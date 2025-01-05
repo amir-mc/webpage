4078e5456058a1bd36f3d8f23178ac1d83a84322
Amir_fooladi <34718321+amir-mc@users.noreply.github.com>
2025-01-05 14:39:33 +0330

    Add files via upload

---
+++ app/(root)/item/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+const Page=()=>{
+    return(
+        <p>
+            salom   
+        </p>
+    )
+}
+export default Page
---
+++ app/(root)/layout.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Layout=({children}:{children:React.ReactNode})=>{
+    return(
+        <>
+        <div>
+        <h1>amin lay</h1>
+        {children}
+        </div>
+        </>
+        
+    )
+}
+export default Layout
